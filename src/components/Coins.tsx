9bb497d072ea0411af5c897c6ba1f05b71bdfe4a
Emad Abaei <emad.ae14@gmail.com>
2025-02-17 10:44:17 +0330

    feat(Coins): add aria rols for seo

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -3,12 +3,16 @@ function Coins() {
     <>
       <img
         src='/coin-1.png'
-        alt='coin image'
+        alt='Rotating gold coin'
+        role='img'
+        aria-label='Rotating gold coin'
         className='max-w-[300px] md:max-w-[400px] absolute bottom-[20%] left-[3%] animate-moverOne'
       />
       <img
         src='/coin-2.png'
-        alt='coin image'
+        alt='Rotating gold coin'
+        role='img'
+        aria-label='Rotating gold coin'
         className='max-w-[300px] md:max-w-[400px] absolute top-[-10%] right-[5%] animate-moverTwo'
       />
     </>
