1dddf422af89f25981585ccee03ab76fd2684675
Emad Abaei <emad.ae14@gmail.com>
2025-03-09 11:40:10 +0330

    fix(Coins): change style

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -1,12 +1,12 @@
 function Coins() {
   return (
     <>
-      <picture className='max-w-[300px] md:max-w-[400px] absolute bottom-[20%] left-[3%] animate-moverOne'>
+      <picture className='max-w-[300px] md:max-w-[400px] absolute bottom-[15%] left-[3%] animate-moverOne'>
         <source srcSet='/coin-1.webp' type='image/webp' />
         <img src='/coin-1.png' alt='Rotating gold coin' />
       </picture>
 
-      <picture className='max-w-[300px] md:max-w-[400px] absolute top-[-10%] right-[5%] animate-moverTwo'>
+      <picture className='max-w-[300px] md:max-w-[400px] absolute top-[10%] right-[5%] animate-moverTwo'>
         <source srcSet='/coin-2.webp' type='image/webp' />
         <img src='/coin-2.png' alt='Rotating gold coin' />
       </picture>
