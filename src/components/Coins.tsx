b44ddc21149389409097bbd53858988b125da1bd
Emad Abaei <emad.ae14@gmail.com>
2025-03-09 10:25:01 +0330

    feat(Coins): add webp images & picture tag

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -1,20 +1,15 @@
 function Coins() {
   return (
     <>
-      <img
-        src='/coin-1.png'
-        alt='Rotating gold coin'
-        role='img'
-        aria-label='Rotating gold coin'
-        className='max-w-[300px] md:max-w-[400px] absolute bottom-[20%] left-[3%] animate-moverOne'
-      />
-      <img
-        src='/coin-2.png'
-        alt='Rotating gold coin'
-        role='img'
-        aria-label='Rotating gold coin'
-        className='max-w-[300px] md:max-w-[400px] absolute top-[-10%] right-[5%] animate-moverTwo'
-      />
+      <picture className='max-w-[300px] md:max-w-[400px] absolute bottom-[20%] left-[3%] animate-moverOne'>
+        <source srcSet='/coin-1.webp' type='image/webp' />
+        <img src='/coin-1.png' alt='Rotating gold coin' />
+      </picture>
+
+      <picture className='max-w-[300px] md:max-w-[400px] absolute top-[-10%] right-[5%] animate-moverTwo'>
+        <source srcSet='/coin-2.webp' type='image/webp' />
+        <img src='/coin-2.png' alt='Rotating gold coin' />
+      </picture>
     </>
   );
 }
